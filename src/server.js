36f55d4dbd26af49bda33988eead52b96ece7365
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-11 13:12:09 +0100

    basic static website

--- src/server.js
+++ src/server.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
-app.use("/books", express.static("books"));
+
+// Routing.
+app.use("/", express.static("wwwroot"));
+app.use("/music", express.static("wwwroot/music"));
+app.use("/books", express.static("wwwroot/books"));
+app.use("/games", express.static("wwwroot/games"));
+
+// Run on port 5001.
 app.listen(5001, () => console.log("Server is online."));
